0ab784645e07b1dc5f4761fb114f36a11c3fdbdd
JasonMillette <jason.millette@maine.edu>
2019-04-18 10:05:52 -0400

    update

--- project/waldo/waldo.py
+++ project/waldo/waldo.py
@@ -10,19 +10,19 @@ import numpy as np
 # Read images
 original = cv2.imread("waldo2.jpeg", cv2.IMREAD_COLOR)
 waldo = cv2.imread("posWaldo.jpeg", cv2.IMREAD_COLOR)
-#w, h = waldo.shape[::-1]
+w, h, channels = waldo.shape
 
 #find matches
 result = cv2.matchTemplate(original, waldo, cv2.TM_CCOEFF)
-min_val, max_val, min_loc, mac_loc = cv2.minMaxLoc(result)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
 #box waldo
-#topLeft = maxLoc
-#botRight = (topLeft[0] + w, topLeft[1] + h)
-#cv2.rectangle(original, top_left, bottom_right, 255, 2)
+top_left = max_loc
+bot_right = (top_left[0] + w, top_left[1] + h)
+cv2.rectangle(original, top_left, bot_right, 255, 2)
 
 #write out image
-cv2.imwrite("out.jpeg", result)
+cv2.imwrite("out.jpeg", original)
 #display
 #cv2.imshow("result", result)
 #cv2.imshow("template", waldo)
